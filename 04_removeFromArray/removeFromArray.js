3d94b577d92f7e2c9ec265097283ed395f5dbcd2
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-09-07 14:42:15 -0300

    Solve exercises 1 through 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,41 @@
-const removeFromArray = function() {
+const removeFromArray = function(arguments1, arguments2) {
 
+    let array = [];
+    let toSkip = [];
+    let toReturn = [];
+
+    for (item of arguments[0]){
+        array.push(item);
+    }
+
+    for (let i = 1; i < arguments.length; i++){
+        toSkip.push(arguments[i]);
+    }
+    
+    for (item of array) {
+        
+        let shouldSkip = false;
+        console.log("Resetting skip status: " + shouldSkip);
+
+        for (skip of toSkip) {
+            if (item === skip){
+                console.log("Skipping: " + item)
+                shouldSkip = true;
+                console.log("Changing skip status: " + shouldSkip);
+                continue;
+            }
+        };
+
+        console.log("Current skip status: " + shouldSkip);
+        if (shouldSkip === false){
+            console.log("Adding: " + item)
+            toReturn.push(item);
+        };
+    }
+
+    return toReturn;
+    
 };
-
+console.log("New array: " + removeFromArray([1, 2, 3, 4], 3, 2));
 // Do not edit below this line
 module.exports = removeFromArray;
