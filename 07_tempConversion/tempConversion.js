ff7f844c3c8e033d58017704b1d51ea933b6bbe7
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-09-07 17:55:16 -0300

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(toCel) {
+  return Number(((toCel - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(toFar) {
+  return Number(((toFar * 9/5) + 32).toFixed(1));
 };
+
+// To Farenheit (x × 9/5 + 32)
+// To Celcius ((x − 32) × 5/9 °C )
 
 // Do not edit below this line
 module.exports = {
