73d05765f3283132806f658940b0cb9a85dbc68b
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-09-07 17:26:06 -0300

    Finish exercises 1 through 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (!string) {return ""}
+    string = String(string);
+    
+    let newString = "";
 
+    for (let i = (string.length -1); i >= 0; i--) {
+        newString += string[i];
+    }
+    return newString;
 };
-
+console.log (reverseString("123"))
 // Do not edit below this line
 module.exports = reverseString;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    // Setup variable to store updated result
+    let result = 0;
 
+    if ( !(Number.isInteger(lastNum)) || !(Number.isInteger(firstNum)) || firstNum < 0 || lastNum < 0 ) {
+        return "ERROR"
+    }
+
+    if (firstNum < lastNum){ 
+        // Sum all integer nums
+        for (let i = firstNum; i <= lastNum; i++){
+            result += i;
+        };
+    }
+    else {
+        for (let i = lastNum; i <= firstNum; i++){
+            result += i;
+        };
+    }
+    return result;
 };
 
 // Do not edit below this line
