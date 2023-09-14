69e799e12c4d428cf058e96b91d52b44e0749f95
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-09-14 15:16:17 -0300

    Complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,54 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
 
+    let currentYear = new Date().getFullYear();
+
+    array.sort((a, b)=> {
+        if (!a.yearOfDeath) {
+            return ((currentYear - a.yearOfBirth) >
+            (b.yearOfDeath - b.yearOfBirth)) ?
+            -1 : 1
+
+        } else if (!b.yearOfDeath) {
+            return ((a.yearOfDeath - a.yearOfBirth) >
+            (currentYear - b.yearOfBirth)) ?
+            -1 : 1
+        }
+
+        return ((a.yearOfDeath - a.yearOfBirth) >
+        (b.yearOfDeath - b.yearOfBirth)) ?
+        -1 : 1
+    
+    });
+    
+    array.forEach((person)=> {
+        if (!person.yearOfDeath) {
+
+        }
+    })
+
+    return array[0];
+    
 };
+
+
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+  // console.log(typeof findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
