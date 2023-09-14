7978c270c88a4986fb15b1626c2b2212bf91c93d
Veru <43966969+VeruAmari@users.noreply.github.com>
2023-09-14 14:04:30 -0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    // Base cases
+    fib = +fib;
+    if (fib < 0) {
+        return 'OOPS'
+    } else if (fib === 1) {
+        return 1;
+    } else if (fib === 0) {
+        return 0;
+    }
 
+    // Start recursion
+    return fibonacci(fib - 1) + fibonacci(fib - 2);
 };
 
 // Do not edit below this line
